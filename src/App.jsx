9e192f5acc351da232a2bf5eae2831969b5d7543
Shefkay <sherifayantoyinbo@gmail.com>
2026-01-20 12:06:21 +0100

    update athkar texts in App component for improved content

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,25 @@ const athkarList = [
   },
   {
     id: 2,
-    text: "",
+    text:
+      `
+    Bismillah, ar-Rahman, ar-Rahim
+    "Qul huwa Allahu ahad, Allahu assamad, Lam yalid walam yoolad, Walam yakun lahu kufuwan ahad"
+    Bismillah, ar-Rahman, ar-Rahim
+    "Qul aAAoothu birabbi alfalaq, Min sharri ma khalaq, Wamin sharri ghasiqin ithawaqab, Wamin sharri annaffathatifee alAAuqad, Wamin sharri hasidin itha hasad"
+    Bismillah, ar-Rahman, ar-Rahim
+    "Qul aAAoothu birabbi annas, Maliki annas, Ilahi annas, Min sharri alwaswasi alkhannas, Allathee yuwaswisu fee sudoori annas, Mina aljinnati wannas"
+    `,
     maxCount: 3
   },
   {
     id: 3,
-    text: "Alhamdulillah",
+    text: "Asbahna wa-Asbaha-mulku lilah walhamdu lillah la ilaha illal-lah, wahdahu la shareeka lah, lahul-mulku walahul-hamd, wahuwa AAala kulli shayin qadeer, rabbi as-aluka khayra ma fee hatha Alyoom, wakhayra ma baAAdah, wa-aAAoothu bika min sharri hatha Alyoom, washarri ma baAAdah, rabbi aAAoothu bika minal-kasal, wasoo-ilkibar, rabbi aAAoothu bika min AAathanbin fin-nar, waAAathabin fil-qabr",
+    maxCount: 1
+  },
+  {
+    id: 4,
+    text: "Allahumma bika asbahna wabika amsayna, wabika nahya, wabika namootu wa-ilaykan-nushoor",
     maxCount: 1
   },
 ]
